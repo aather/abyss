569a2d59eeb2e5034d6201d4739641d21141510b
Amer Ather <aather@netflix.com>
2015-05-29 00:22:04 -0700

    update

--- startBenchmarks.sh
+++ startBenchmarks.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+killall()
+{
+ kill  `pgrep systats.pl`   2>/dev/null
+ kill  `pgrep tcpstats.pl`  2>/dev/null
+ kill  `pgrep iolatency.pl` 2>/dev/null
+ kill  `pgrep cassandra.pl` 2>/dev/null
+ kill  `ps -elf|grep CLOUDSTAT|grep -v grep |awk '{print $4}'` 2>/dev/null
+
+for PID in $PIDLIST
+ do
+        kill -9 $PID 2>/dev/null
+ done
+exit
+}
+
+trap killall HUP INT QUIT KILL TERM USR1 USR2 EXIT
+
 # check if it is centOS, then install netcat 'nc' package 
 if [ -f "/usr/bin/yum" ]
 then
@@ -9,6 +26,7 @@ fi
 # Agent to monitor system stats: cpu, io, disk, net, mem
 cd monitor
 nohup ./loop-systats.sh &
+PIDLIST="$PIDLIST $!"
 cd ..
 
 # Agent to monitor Storage IO latencies
@@ -17,6 +35,7 @@ if [ -f "/usr/bin/perf" ]
 then
  cd sniffer
  nohup ./loop-iolatency.sh &
+ PIDLIST="$PIDLIST $!"
  cd ..
 else
   echo "perf is not available. iolatency agent is not started"
@@ -27,6 +46,7 @@ if [ -f "/usr/bin/make" ]
 then
    cd sniffer
    nohup ./loop-tcpstats.sh &
+   PIDLIST="$PIDLIST $!"
    cd ..
 fi
 
@@ -34,9 +54,11 @@ fi
 cd test-suites/net-tests/
 while :
 do
- nohup ./netBW.pl&             		# Network throughput tests
- nohup ./netTPS.pl &       		# Next two network latency tests starts together
+ nohup ./netTPS.pl &       		# Run network latency tests nonstop. Low net overhead!
+ PIDLIST="$PIDLIST $!"
  nohup ./pingRTT.pl & 
- nohup ./memcachedRTT.pl		# memcache RPS test
+ PIDLIST="$PIDLIST $!"
+ nohup ./netBW.pl             		# Start Network throughput test
+ nohup ./memcachedRTT.pl		# When Net throughput test ends start memcache test
 done
 
--- startMonitoring.sh
+++ startMonitoring.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+killall()
+{
+ kill  `pgrep systats.pl`   2>/dev/null
+ kill  `pgrep tcpstats.pl`  2>/dev/null
+ kill  `pgrep iolatency.pl` 2>/dev/null
+ kill  `pgrep cassandra.pl` 2>/dev/null
+ kill  `ps -elf|grep CLOUDSTAT|grep -v grep |awk '{print $4}'` 2>/dev/null
+
+for PID in $PIDLIST
+ do
+        kill -9 $PID 2>/dev/null
+ done
+exit
+}
+
+trap "killall" HUP INT QUIT KILL TERM USR1 USR2 EXIT
+
 # check if it is centOS, then install netcat 'nc' package. It is not installed on CentOS instances 
 if [ -f "/usr/bin/yum" ] 
 then
@@ -9,6 +26,7 @@ fi
 # Agent to monitor system stats: cpu, io, disk, net, mem
 cd monitor
 nohup ./loop-systats.sh &
+PIDLIST="$PIDLIST $!"
 cd ..
 
 # Agent to monitor Storage IO latencies
@@ -17,6 +35,7 @@ if [ -f "/usr/bin/perf" ]
 then
  cd sniffer
  nohup ./loop-iolatency.sh &
+ PIDLIST="$PIDLIST $!"
  cd ..
 else
   echo "perf is not available. iolatency agent is not started"
@@ -27,6 +46,7 @@ if [ -f "/usr/bin/make" ]
 then
    cd sniffer
    nohup ./loop-tcpstats.sh &
+   PIDLIST="$PIDLIST $!"
    cd ..
 fi
 
@@ -36,6 +56,8 @@ if [ -d "/apps/nfcassandra_server" ]
 then
  cd app/cassandra
  nohup ./loop-cassandra.sh &
+ PIDLIST="$PIDLIST $!"
  cd ../..
 fi
 
+wait
