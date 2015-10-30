194d65497002b8ce5062f3434c5d8d3ab59a972b
Amer Ather <aather@netflix.com>
2015-10-30 14:37:28 -0700

    update

---
+++ graphite-setup/TEMP/grafana/test/specs/row-ctrl-specs.js
@@ -0,0 +1,4 @@
+/*! grafana - v1.9.0 - 2014-12-02
+ * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
+
+define(["./helpers","controllers/row"],function(a){describe("RowCtrl",function(){var b=new a.ControllerTestContext;beforeEach(module("grafana.controllers")),beforeEach(b.providePhase()),beforeEach(b.createControllerPhase("RowCtrl"))})});
